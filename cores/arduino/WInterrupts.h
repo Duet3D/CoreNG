728523de80110481593f5ad0a1ca3a9dc01418d9
David Crocker <dcrocker@eschertech.com>
2017-07-25 10:59:37 +0100

    Changes to comments only

--- cores/arduino/WInterrupts.h
+++ cores/arduino/WInterrupts.h
@@ -33,7 +33,7 @@ void detachInterrupt(uint32_t pin);
 }
 #endif
 
-// Return true id we are in an interrupt service routine
+// Return true if we are in an interrupt service routine
 bool inInterrupt();
 
 #endif /* _WIRING_INTERRUPTS_ */
