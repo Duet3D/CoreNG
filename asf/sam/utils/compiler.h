2c234a6d9050bec0f7f9e4f2ec87faf71581750a
David Crocker <dcrocker@eschertech.com>
2020-04-27 13:51:50 +0100

    Got rid of unwanted includes in compiler.h

--- asf/sam/utils/compiler.h
+++ asf/sam/utils/compiler.h
@@ -60,10 +60,16 @@
 
 #ifndef __ASSEMBLY__ // Not defined for assembling.
 
+#if 1	// DC
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+#else
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+#endif
 
 #ifdef __ICCARM__
 /*! \name Compiler Keywords
@@ -225,7 +231,7 @@
 
 /* Define OPTIMIZE_HIGH attribute */
 #if defined   ( __CC_ARM   ) /* Keil µVision 4 */
-#   define OPTIMIZE_HIGH _Pragma("O3") 
+#   define OPTIMIZE_HIGH _Pragma("O3")
 #elif defined ( __ICCARM__ ) /* IAR Ewarm 5.41+ */
 #   define OPTIMIZE_HIGH _Pragma("optimize=high")
 #elif defined (  __GNUC__  ) /* GCC CS3 2009q3-68 */
@@ -856,7 +862,7 @@ typedef struct
 #define  LSB2W(u32)     MSB1W(u32)           //!< Least significant byte of 3rd rank of \a u32.
 #define  LSB1W(u32)     MSB2W(u32)           //!< Least significant byte of 2nd rank of \a u32.
 #define  LSB0W(u32)     MSB3W(u32)           //!< Least significant byte of 1st rank of \a u32.
-        
+
 #define  MSW(u64)       (((U32 *)&(u64))[1]) //!< Most significant word of \a u64.
 #define  LSW(u64)       (((U32 *)&(u64))[0]) //!< Least significant word of \a u64.
 #define  MSH0(u64)      (((U16 *)&(u64))[3]) //!< Most significant half-word of 1st rank of \a u64.
