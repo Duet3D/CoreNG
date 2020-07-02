ed8d88dda556a19d1493b8e51aaaccf1ca685d05
likha3d <dev@likha3d.com>
2020-07-02 15:42:18 +0800

    Add instead of remove driver files

--- variants/duetNG/variant.cmake
+++ variants/duetNG/variant.cmake
@@ -3,7 +3,12 @@ list(APPEND SRCS
     "${CMAKE_CURRENT_LIST_DIR}/system_sam4e.c"
     "${CMAKE_CURRENT_LIST_DIR}/startup_sam4e.c"
     "${CMAKE_CURRENT_LIST_DIR}/exceptions.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/asf/common/services/clock/sam4e/sysclk.c")
+
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/common/services/clock/sam4e/sysclk.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/rswdt/rswdt.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/afec/afec.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/dmac/dmac.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/cmcc/cmcc.c")
 
 list(APPEND INCLUDE_DIRS
     "${CMAKE_CURRENT_LIST_DIR}"
--- variants/sam4s/variant.cmake
+++ variants/sam4s/variant.cmake
@@ -1,17 +1,11 @@
 list(APPEND SRCS
-    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/adc/adc.c"
     "${CMAKE_CURRENT_LIST_DIR}/variant.cpp"
     "${CMAKE_CURRENT_LIST_DIR}/system_sam4s.c"
     "${CMAKE_CURRENT_LIST_DIR}/startup_sam4s.c"
     "${CMAKE_CURRENT_LIST_DIR}/exceptions.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/asf/common/services/clock/sam4s/sysclk.c")
 
-list(REMOVE_ITEM SRCS
-    "asf/sam/drivers/rswdt/rswdt.c"
-    "asf/sam/drivers/afec/afec.c"
-    "asf/sam/drivers/dmac/dmac.c"
-    "asf/sam/drivers/cmcc/cmcc.c"
-)
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/common/services/clock/sam4s/sysclk.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/adc/adc.c")
 
 list(APPEND INCLUDE_DIRS
     "${CMAKE_CURRENT_LIST_DIR}"
