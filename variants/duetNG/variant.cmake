a7bcdd2084882cb08aa4aff227062a4d54b2e462
likha3d <dev@likha3d.com>
2020-07-02 17:17:19 +0800

    Add instead of remove driver files

--- variants/duetNG/variant.cmake
+++ variants/duetNG/variant.cmake
@@ -8,10 +8,15 @@ list(APPEND SRCS
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/rswdt/rswdt.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/afec/afec.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/dmac/dmac.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/cmcc/cmcc.c")
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/cmcc/cmcc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/twi/twi.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/pdc/pdc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/udp/udp_device.c"
+)
 
 list(APPEND INCLUDE_DIRS
     "${CMAKE_CURRENT_LIST_DIR}"
-    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/utils/cmsis/sam4e/include")
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/utils/cmsis/sam4e/include"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/dmac")
 
 set(LINKER_SCRIPTS ${CMAKE_CURRENT_LIST_DIR}/linker_scripts/gcc/flash.ld)
--- variants/sam4s/variant.cmake
+++ variants/sam4s/variant.cmake
@@ -5,7 +5,11 @@ list(APPEND SRCS
     "${CMAKE_CURRENT_LIST_DIR}/exceptions.c"
 
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/common/services/clock/sam4s/sysclk.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/adc/adc.c")
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/adc/adc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/pdc/pdc.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/twi/twi.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/udp/udp_device.c"
+)
 
 list(APPEND INCLUDE_DIRS
     "${CMAKE_CURRENT_LIST_DIR}"
