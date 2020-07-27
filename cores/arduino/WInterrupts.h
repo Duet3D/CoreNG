a6e265e9437e7b62de8abacf5e23791021fee77b
David Crocker <dcrocker@eschertech.com>
2020-07-27 20:49:58 +0100

    Added cache management and base priority functions

--- cores/arduino/WInterrupts.h
+++ cores/arduino/WInterrupts.h
@@ -48,7 +48,7 @@ bool inInterrupt() noexcept;
 class AtomicCriticalSectionLocker
 {
 public:
-	AtomicCriticalSectionLocker() : flags(cpu_irq_save())
+	AtomicCriticalSectionLocker() noexcept : flags(cpu_irq_save())
 	{
 	}
 
@@ -60,6 +60,28 @@ public:
 private:
 	irqflags_t flags;
 };
+
+// Functions to change the base priority, to shut out interrupts up to a priority level
+
+// Get the base priority and shut out interrupts lower than or equal to a specified priority
+inline uint32_t ChangeBasePriority(uint32_t prio) noexcept
+{
+	const uint32_t oldPrio = __get_BASEPRI();
+	__set_BASEPRI_MAX(prio << (8 - __NVIC_PRIO_BITS));
+	return oldPrio;
+}
+
+// Restore the base priority following a call to ChangeBasePriority
+inline void RestoreBasePriority(uint32_t prio) noexcept
+{
+	__set_BASEPRI(prio);
+}
+
+// Set the base priority when we are not interested in the existing value i.e. definitely in non-interrupt code
+inline void SetBasePriority(uint32_t prio) noexcept
+{
+	__set_BASEPRI(prio << (8 - __NVIC_PRIO_BITS));
+}
 
 #endif
 
